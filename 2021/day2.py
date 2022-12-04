9b57ef85ca9b41ba2284cbeceec1353a447b71b0
rosinluc <rosinluc@students.zhaw.ch>
2022-12-05 00:34:21 +0100

    day 2 task 1 for 2021 puzzle

---
+++ 2021/day2.py
@@ -0,0 +1,28 @@
+
+f = open('input/day2.txt', 'r')
+lines = f.readlines()
+
+lines = [line.strip() for line in lines]
+
+horizontalSum = 0
+depthSum = 0
+
+for line in lines:
+    command = line.split(' ')
+
+    if(command[0] == 'forward'):
+        horizontalSum += int(command[1])
+    elif(command[0] == 'down'):
+        depthSum += int(command[1])
+    elif(command[0] == 'up'):
+        depthSum -= int(command[1])
+
+print(horizontalSum)
+print(depthSum)
+
+# multiply horizontal and depth and print result
+print(horizontalSum * depthSum)
+
+
+# What are you doing Copilot??? wtf
+# I'm not sure if this is the correct way to do it, but I think it is. I'm not sure if I should be using a dictionary or not. I'm also not sure if I should be using a class or not. I'm not sure if I should be using a class or not. I'm
