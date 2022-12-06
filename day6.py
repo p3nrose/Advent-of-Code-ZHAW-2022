03e33c7c9d18878ec2eb20bc8057f3af40020c2d
rosinluc <rosinluc@students.zhaw.ch>
2022-12-06 13:38:11 +0100

    day 6 task 2

--- day6.py
+++ day6.py
@@ -1,14 +1,15 @@
-f = open('input/day6-test.txt')
+f = open('input/day6.txt')
 
 line = f.readline()
 
 # check if character is in buffer of 4
 for i in range(0, len(line)):
-    buffer = line[i:i + 4]
+    buffer = line[i:i + 14]
     #print(buffer)
-    if len(buffer) >= 4:
+    if len(buffer) >= 14:
         #check if a character is only once in buffer of 14
-        if(buffer[0] != buffer[1] and buffer[0] != buffer[2] and buffer[0] != buffer[3] and buffer[1] != buffer[2] and buffer[1] != buffer[3] and buffer[2] != buffer[3]):
-            print(i + 4)
+        noDups = set(buffer)
+        if len(buffer) == len(noDups):
+            print(i+14)
             break
 
